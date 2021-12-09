e27f7898781bba724ea9e6fd65c31cef1881b955
xiut1 <!Dpffltm1>
2021-12-09 12:17:52 +0900

    [HJ] serviceComponent.vue save

--- src/components/views/serviceComponent.vue
+++ src/components/views/serviceComponent.vue
@@ -5,30 +5,27 @@
 
     <div class="services_container container grid">
       <!-- services 1 -->
-      <div class="services_content">
+      <div class="services_content" v-for="( value, index ) in services" :key="index">
         <div>
-          <uil-web-grid class="services_icon" />
-          <h3 class="services_title">Ui / Ux <br> Designer</h3>
+          <uil-web-grid class="services_icon"/>
+          <h3 class="services_title"> {{ value.content.title }} </h3>
         </div>
 
         <span class="button button-flex button-samll button-link services_button">
           View More
-          <uil-arrow-right class="button_icon" />
+          <uil-arrow-right class="button_icon"/>
         </span>
 
         <div class="services_modal">
           <div class="services_modal-content">
-            <h4 class="services_modal-title">Ui / Ux <br> Designer</h4>
-            <uil-times class="services_modal-close" />
+            <h4 class="services_modal-title"> {{ value.content.subtitle }} </h4>
+            <uil-times class="services_modal-close"/>
 
             <ul class="services_modal-services grid">
+<!--              <li class="services_modal-service" v-for="( index ) in services.content.info" :key="index">-->
               <li class="services_modal-service">
-                <uil-check-circle class="services_modal-icon" />
-                <p>I develop the user interface.</p>
-              </li>
-              <li class="services_modal-service">
-                <uil-check-circle class="services_modal-icon" />
-                <p>I develop the user dickpunch.</p>
+                <uil-check-circle class="services_modal-icon"/>
+                <p> {{ index.message }} </p>
               </li>
             </ul>
           </div>
@@ -48,12 +45,39 @@ import {
 
 export default {
   name: "serviceComponent",
+  data() {
+    return {
+      services: [
+        {
+          content: {
+            title: 'Ui / Ux Designer 1',
+            subtitle: 'Ui / Ux 1',
+            info: [
+              { message: 'foo' },
+              { message: 'boo' }
+            ]
+          }
+        },
+        {
+          content: {
+            title: 'Ui / Ux Designer 2',
+            subtitle: 'Ui / Ux 2',
+            info: [
+              { message: 'blah blah blah blah ' },
+              { message: 'blah blah blah blah blah blah blah blah ' }
+            ]
+          }
+        }
+      ]
+    }
+  },
   components: {
     UilWebGrid,
     UilArrowRight,
     UilTimes,
     UilCheckCircle
-  }
+  },
+  methods: {}
 }
 </script>
 
